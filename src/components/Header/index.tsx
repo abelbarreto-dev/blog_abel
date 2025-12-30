06304b0f7ddf44b98dbf87aed61c3a45ceaecb7b
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-30 17:07:53 -0300

    feat: adding breakpoints to a component to be responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,16 @@
-"use client";
-
 import clsx from "clsx";
 
 export const Header = () => {
     return (
-        <h1
-            className={clsx([
-                "text-6xl",
-                "font-bold text-blue-500",
-                "hover:text-blue-50",
-                "hover:bg-blue-500 cursor-pointer",
-                "transition duration-300",
-            ])}
-            onClick={() => alert(123)}
-        >
-            Texto do meu Título
-        </h1>
+        <header>
+            <h1 className={clsx(
+                "text-4xl/normal font-extrabold py-8",
+                "sm:text-5xl sm:py-10",
+                "md:text-6xl md:py-11",
+                "lg:text-7xl md:py-12"
+            )}>
+                <a href="">The Abel Blog</a>
+            </h1>
+        </header>
     );
 };
