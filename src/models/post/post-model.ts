6acbd1a8b425e638981401d14fe77a5cf96675c1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-30 13:31:25 -0300

    feat: created model for post

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export type PostModel = {
+    id: string;
+    title: string;
+    slug: string;
+    excerpt: string;
+    content: string;
+    coverImageUrl: string;
+    author: string;
+    published: boolean;
+    createdAt: string;
+    updatedAt: string;
+};
