79d8b8d1c9436dd225764e52c054e9ef91d3b6a4
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-30 14:24:47 -0300

    feat: implement find by id in json post repository

--- src/repositories/post/json-post-reposiroty.ts
+++ src/repositories/post/json-post-reposiroty.ts
@@ -1,16 +1,40 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+    ROOT_DIR,
+    "src",
+    "db",
+    "seed",
+    "posts.json"
+);
+const ENCODE = "utf-8";
 
 export class JsonPostRepository implements PostRepository {
-    private async readFromDisk() {}
+    private async readFromDisk() {
+        const jsonContent = await readFile(JSON_POSTS_FILE_PATH, ENCODE);
+
+        const { posts } = JSON.parse(jsonContent);
+
+        return posts;
+    }
 
     async findAll(): Promise<PostModel[]> {
-        throw new Error("Method not implemented.");
+        return this.readFromDisk() || [];
+    }
+
+    async findById(id: string): Promise<PostModel> {
+        const posts = await this.findAll();
+
+        const foundPost = posts.find(post => post.id === id);
+
+        if (!foundPost) throw new Error("Post Not Found");
+
+        return foundPost;
     }
 }
 
-export const postRepository = new JsonPostRepository();
-
-console.log(ROOT_DIR);
+export const postRepository: PostRepository = new JsonPostRepository();
