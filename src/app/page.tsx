15cc0378ada704fb52c3fea5d6eea499225a16d1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2026-01-01 16:21:58 -0300

    feat: created post feature component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,16 @@ import { Header } from "@/components/Header";
 import { PostLists } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
-import { PostHeading } from "@/components/PostHeading";
-import { ImageCover } from "../components/ImageCover/index";
+import { PostFeature } from "@/components/PostFeature";
 
 export default async function HomePage() {
     return (
         <Container>
             <Header />
 
-            <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-                <ImageCover
-                    src={"/images/bryen_0.png"}
-                    url={""}
-                    priority={true}
-                />
-
-                <div className="flex flex-col gap-4 sm:justify-center">
-                    <time
-                        dateTime={new Date().toDateString()}
-                        className="text-slate-600 block text-sm/tight"
-                    >
-                        {new Date().toLocaleDateString()}
-                    </time>
-
-                    <PostHeading url={""} as={"h1"}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </PostHeading>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dicta quia quidem voluptatum sapiente saepe. Excepturi
-                        soluta corporis fuga, sit commodi libero ipsum harum
-                        aliquam suscipit veritatis rerum! Vel magni, voluptas
-                        harum earum ipsa assumenda corrupti qui libero ducimus
-                        numquam? Quia hic odio maxime? Ea voluptatem excepturi
-                        voluptate optio minus rem eveniet nihil quia quae totam
-                        obcaecati ex, cum tenetur quas? Quaerat odit debitis, ut
-                        porro reprehenderit eum dolore ipsa sed veniam
-                        necessitatibus iure quis maxime ipsum illum voluptas
-                        vitae vel? Accusantium nemo sapiente placeat ex facilis,
-                        ratione animi possimus assumenda vel nam cumque facere
-                        odit. Necessitatibus inventore fugit aut nulla!
-                    </p>
-                </div>
-            </section>
+            <Suspense fallback={<SpinLoader />}>
+                <PostFeature />
+            </Suspense>
 
             <Suspense fallback={<SpinLoader />}>
                 <PostLists />
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -8,11 +8,13 @@ export const PostLists = async () => {
     return (
         <div className="gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {posts.map((post) => {
+                const postLink = `/post/${post.slug}`;
+
                 return (
                     <section className="flex flex-col group gap-4" key={post.id}>
                         <ImageCover
                             src={post.coverImageUrl}
-                            url={`/post/${post.slug}`}
+                            url={postLink}
                             priority={false}
                         />
 
