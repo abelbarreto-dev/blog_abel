39e0a4272569e771d07448882777cc6a96b857a0
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2026-01-03 13:19:16 -0300

    feat: 9

--- src/components/SafeMarkdown/inde.tsx
+++ src/components/SafeMarkdown/inde.tsx
@@ -9,21 +9,34 @@ type SafeMarkdownProps = {
 
 export const SafeMarkdown = ({ markdown }: SafeMarkdownProps) => {
     return (
-        <div className={clsx(
-            "prose prose-slate",
-            "w-full max-w-none",
-            "overflow-hidden",
-            "prose-a:transition",
-            "prose-a:no-underline",
-            "prose-a:text-blue-500",
-            "prose-a:hover:text-blue-700",
-            "prose-a:hover:underline",
-            "prose-img:mx-auto",
-            "md:prose-lg",
-        )}>
+        <div
+            className={clsx(
+                "prose prose-slate",
+                "w-full max-w-none",
+                "overflow-hidden",
+                "prose-a:transition",
+                "prose-a:no-underline",
+                "prose-a:text-blue-500",
+                "prose-a:hover:text-blue-700",
+                "prose-a:hover:underline",
+                "prose-img:mx-auto",
+                "md:prose-lg"
+            )}
+        >
             <ReactMarkdown
                 rehypePlugins={[rehypeSanitize]}
                 remarkPlugins={[remarkGfm]}
+                components={{
+                    table: ({node, ...props}) => {
+                        if (!node?.children) return "";
+
+                        return (
+                            <div className="overflow-x-auto">
+                                <table className="w-full min-w-150" {...props} />
+                            </div>
+                        );
+                    },
+                }}
             >
                 {markdown}
             </ReactMarkdown>
