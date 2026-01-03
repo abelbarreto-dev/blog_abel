15fee6bd9e6b5d5e9bb857354d12d60f3e6522c2
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2026-01-03 10:10:10 -0300

    feat: generate custom metadata with generate metadata function

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type Slug = {
@@ -7,6 +8,19 @@ type Slug = {
 
 type PostSlugProps = {
     params: Promise<Slug>;
+};
+
+export const generateMetadata = async ({
+    params,
+}: PostSlugProps): Promise<Metadata> => {
+    const { slug } = await params;
+
+    const post = await findPostBySlugCached(slug).catch(() => notFound());
+
+    return {
+        title: post.title,
+        description: post.excerpt,
+    };
 };
 
 export default async function PostSlugPage({ params }: PostSlugProps) {
