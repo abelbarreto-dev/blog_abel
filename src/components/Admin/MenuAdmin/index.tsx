80f6323a4515e82e607ac5412915e5d73201ea14
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-13 17:01:40 -0300

    feat: admin nav structure created

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,11 +1,33 @@
+import clsx from "clsx";
+import { FileTextIcon, HouseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin () {
-    return (
-        <nav>
-            <a href="/" target="_blank">Home</a>
+    const navClasses = clsx(
+        "bg-slate-900 text-slate-100 rounded-lg",
+        "flex flex-col mb-8",
+        "h-10",
+        "sm:flex-row sm:flex-wrap"
+    );
 
-            <Link href={"/admin/post"}>Posts</Link>
+    const linkClasses = clsx(
+        "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
+        "flex items-center justify-start gap-2",
+        "transition hover:bg-slate-800",
+        "rounded-lg",
+        "h-10",
+        "shrink-0"
+    );
+
+    return (
+        <nav className={navClasses}>
+            <a href="/" target="_blank" className={linkClasses}>
+            <HouseIcon />
+            Home</a>
+
+            <Link href={"/admin/post"} className={linkClasses}>
+            <FileTextIcon />
+            Posts</Link>
         </nav>
     );
 }
