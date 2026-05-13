030c0d6b8befd475407af1e4625da4546ad3b4ff
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-12 22:29:57 -0300

    feat: creating layout for admin posts context

---
+++ src/app/admin/post/layout.tsx
@@ -0,0 +1,15 @@
+import { MenuAdmin } from "@/components/Admin/MenuAdmin";
+
+export default function AdminPostLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <MenuAdmin />
+
+            {children}
+        </>
+    );
+}
