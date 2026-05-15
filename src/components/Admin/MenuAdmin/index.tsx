8bccbf05eeed3d87dadda36f445446a124245a3b
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-15 12:42:42 -0300

    feat: create menu for mobile context

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import clsx from "clsx";
-import { FileTextIcon, HouseIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin () {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        setIsOpen(false);
+    }, [pathname]);
+
     const navClasses = clsx(
         "bg-slate-900 text-slate-100 rounded-lg",
         "flex flex-col mb-8",
-        "h-10",
-        "sm:flex-row sm:flex-wrap"
+        "sm:flex-row sm:flex-wrap",
+        !isOpen && "h-10 overflow-hidden",
+        "sm:overflow-visble sm:h-auto",
     );
 
     const linkClasses = clsx(
@@ -16,11 +28,34 @@ export function MenuAdmin () {
         "transition hover:bg-slate-800",
         "rounded-lg",
         "h-10",
-        "shrink-0"
+        "shrink-0",
+        "cursor-pointer",
+    );
+
+    const openCloseBtnClasses = clsx(
+        linkClasses,
+        "text-blue-200 italic",
+        "sm:hidden"
     );
 
     return (
         <nav className={navClasses}>
+            <button className={openCloseBtnClasses} onClick={() => setIsOpen(s => !s)}>
+                {!isOpen && (
+                    <>
+                        <MenuIcon />
+                        Menu
+                    </>
+                )}
+
+                {isOpen && (
+                    <>
+                        <CircleXIcon />
+                        Fechar
+                    </>
+                )}
+            </button>
+
             <a href="/" target="_blank" className={linkClasses}>
             <HouseIcon />
             Home</a>
@@ -28,6 +63,11 @@ export function MenuAdmin () {
             <Link href={"/admin/post"} className={linkClasses}>
             <FileTextIcon />
             Posts</Link>
+
+            <Link href={"/admin/post/new"} className={linkClasses}>
+            <PlusIcon />
+            Criar Post
+            </Link>
         </nav>
     );
 }
