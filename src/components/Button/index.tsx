63fb5eaef41c827b640af19b0a83d56a85a2e7a7
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-15 14:30:17 -0300

    feat: mapping button variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,32 @@
-type ButtonProps = {} & React.ComponentProps<"button">;
+import clsx from "clsx";
+
+type ButtonVariants = "default" | "ghost" | "danger";
+
+type ButtonSizes = "sm" | "md" | "lg";
+
+type ButtonProps = {
+    variant: ButtonVariants;
+    size: ButtonSizes;
+} & React.ComponentProps<"button">;
 
 export function Button({
-    children, ...props
+    children, variant, size="md" ...props
 }: ButtonProps) {
+    const buttonVariants: Record<ButtonVariants, string> = {
+        default: clsx("bg-blue-600 text-blue-100"),
+        ghost: clsx("bg-slate-200 text-slate-900"),
+        danger: clsx("bg-red-600 text-red-100"),
+    };
+
+    const buttonSizes: Record<ButtonSizes, string> = {
+        sm: clsx(),
+        md: clsx(),
+        lg: clsx()
+    };
+
+    const buttonCLasses = clsx(buttonVariants[variant], buttonSizes[size]);
+
     return (
-        <button {...props}>{children}</button>
+        <button className={buttonCLasses} {...props}>{children}</button>
     );
 }
