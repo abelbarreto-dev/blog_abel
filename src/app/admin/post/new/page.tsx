d6844470611924b0023fe62ee35d9af270fd9dfd
abelbarreto-dev <abelbcarvalho@gmail.com>
2026-05-16 11:04:56 -0300

    feat: creating element input text

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/Button";
+import { InputText } from "@/components/InputText";
 import { HeartIcon } from "lucide-react";
 
 export default async function AdminAddPostPage() {
@@ -8,6 +9,15 @@ export default async function AdminAddPostPage() {
                 <Button variant="default" size="sm"><HeartIcon />Mire</Button>
                 <Button variant="ghost" size="md"><HeartIcon />Mire</Button>
                 <Button variant="danger" size="lg"><HeartIcon />Mire</Button>
+            </div>
+
+            <div className="flex flex-col gap-6">
+                <InputText labelText="Abel" placeholder="Mirian, eu te amo!" />
+                <InputText labelText="Abel" placeholder="Mirian, eu te amo!" />
+
+                <InputText labelText="Abel" placeholder="Mirian, eu te amo!" disabled/>
+                <InputText labelText="Abel" defaultValue="Mirian, eu te amo!" placeholder="Mirian, eu te amo!" disabled/>
+                <InputText labelText="Abel" placeholder="Mirian, eu te amo! Reading..." readOnly/>
             </div>
 
             <div className="py-16 text-6xl flex flex-wrap items-center gap-4">
